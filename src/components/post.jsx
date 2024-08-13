e4b19c703eb0eb48553f4b545275f28c38ed587f
Kehan Zhang <kehan9@gmail.com>
2024-08-13 14:26:29 -0700

    LP4: Creating a like button

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import myAsset from '../assets/my_asset.jpeg';
+import { Heart } from 'lucide-react';
 
 function Post() {
   return (
     <div className='p-2'>
       <img src={myAsset} alt="my asset" />
       <p>This is a post</p>
+      <Heart />
+      <Heart fill="red" color="red" />
     </div>
   );
 };
