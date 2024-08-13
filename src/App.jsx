92ea0ceb3b627cfa9bdfdbf5e1bc90a58c5fe342
Kehan Zhang <kehan9@gmail.com>
2024-08-13 12:27:15 -0700

    LP1: Text elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ function App() {
   return (
     <div className='min-h-screen w-screen flex flex-col justify-center items-center'>
       <img src={athensLogo}  alt="Athens logo" className="h-24 w-24 my-2" />
-      <p className='text-2xl font-bold'>Welcome to Athens</p>
-      <p className='text-lg text-gray-600'>Course: Build Instagram clone with react</p>
+      <p className='text-2xl font-bold'>hello world</p>
+      <p className='text-lg text-gray-600'>wow this is so cool!</p>
       <div className='pt-4'>
         <p>
           Edit <code>src/App.jsx</code> and save to begin!
         </p>
       </div>
+      <p>this text won't be styled</p>
     </div>
   )
 }
