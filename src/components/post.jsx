27ef25516d63faefd6aa212ebe361ae6b89cbe12
Kehan Zhang <kehan9@gmail.com>
2024-08-13 17:13:08 -0700

    LP6: A peak into React's powers

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import myAsset from '../assets/my_asset.jpeg';
 import { Heart } from 'lucide-react';
 
 function Post() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikePressed = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className='p-2'>
       <img src={myAsset} alt="my asset" />
       <p>This is a post</p>
-      <Heart />
-      <Heart fill="red" color="red" />
+      <div onClick={handleLikePressed}>
+        {isLiked ? <Heart fill="red" color="red" /> : <Heart />}
+      </div>
     </div>
   );
 };
