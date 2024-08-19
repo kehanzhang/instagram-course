b3f79f8da614ac4eb0570a379a3be56dec61d5ff
Kehan Zhang <kehan9@gmail.com>
2024-08-18 17:23:39 -0700

    LP32: Adding styling, Profile Picture and Navigation

--- src/components/EditableProfileAttribute.jsx
+++ src/components/EditableProfileAttribute.jsx
@@ -4,12 +4,15 @@ const EditableProfileAttribute = ({ attribute, state, updateState }) => {
   return (
     <div className="flex w-full">
       <p className="w-1/4">{attribute}</p>
-      <input
-        type="text"
-        value={state}
-        onChange={(e) => updateState(e.target.value)}
-        className="w-3/4 focus:outline-none"
-      />
+      <div className='w-3/4 border-b border-gray-100 pb-2'>
+        <input
+            type="text"
+            value={state}
+            onChange={(e) => updateState(e.target.value)}
+            className="focus:outline-none"
+        />
+      </div>
+
     </div>
   );
 };
--- src/screens/editProfileScreen.jsx
+++ src/screens/editProfileScreen.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import useSWR from 'swr';
+import { Link } from 'react-router-dom';
+import { ChevronLeft } from 'lucide-react';
 import EditableProfileAttribute from '../components/EditableProfileAttribute';
+import Avatar from '../components/avatar';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -26,23 +29,34 @@ const EditProfileScreen = () => {
   if (!profileData) return <div>Loading...</div>;
 
   return (
-    <div className="w-full p-4">
-      <p className="text-center">Edit Profile</p>
-      <EditableProfileAttribute
-        attribute="Name"
-        state={name}
-        updateState={setName}
-      />
-      <EditableProfileAttribute
-        attribute="Username"
-        state={username}
-        updateState={setUsername}
-      />
-      <EditableProfileAttribute
-        attribute="Bio"
-        state={bio}
-        updateState={setBio}
-      />
+    <div className="w-full">
+      <div className="flex items-center p-4 border-b border-gray-100">
+        <Link to="/profile" className='absolute left-4'>
+          <ChevronLeft />
+        </Link>
+        <p className="flex-grow text-center font-bold">Edit Profile</p>
+      </div>
+      <div className="flex flex-col items-center py-2 border-b border-gray-100">
+        <Avatar src={profileData.profile_pic_url} size="large" />
+        <p className="mt-2 text-blue-500 text-sm font-semibold">Edit Picture</p>
+      </div>
+      <div className="p-4 space-y-2">
+        <EditableProfileAttribute
+          attribute="Name"
+          state={name}
+          updateState={setName}
+        />
+        <EditableProfileAttribute
+          attribute="Username"
+          state={username}
+          updateState={setUsername}
+        />
+        <EditableProfileAttribute
+          attribute="Bio"
+          state={bio}
+          updateState={setBio}
+        />
+      </div>
     </div>
   );
 };
